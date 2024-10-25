faeb083a83114e0019929006ea2057694df112ca
amrutha <amruthaburidi321@gmail.com>
2024-10-25 14:56:20 +0530

    amrutha

---
+++ final_24/urls.py
@@ -0,0 +1,26 @@
+"""final_24 URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.conf.urls import url
+from django.contrib import admin
+from Application1 import views
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^a1/', views.sample1,name='web1'),
+    url(r'^a3/', views.sample3),
+    url(r'^a4/', views.sample3,name='web2'),
+    
+]
